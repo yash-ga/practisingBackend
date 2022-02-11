6b5f97576cadde9fb1888d0aa17e98c6a9976456
Yash Garg <yash_fw10_103@masai.school>
2022-02-11 20:22:01 +0530

    basic crud setup made

--- practiseDay1/server.js
+++ practiseDay1/server.js
@@ -4,20 +4,37 @@ const mongoose=require('mongoose')
 const connect=()=>{
    return mongoose.connect('https/mongodb://127.0.0.1:27017/user')
 }
-const UserSchema=mongoose.Schema({
+const UserSchema= new mongoose.Schema({
     firstName:{type:String,required:true},
     lastName:{type:String,required:false},
     dob:{type:Number,required:true}
 
 })
 const User=mongoose.model("users",UserSchema)
-const app=app.use()
-app.json()
+const app=express()
+app.use(express.json())
 
 app.get("/users",async(req,res)=>{
 
-    const User=await User.get(req.body)
-    res.status("200").send({User})
+    const user=await User.create(req.body)
+   return res.status("200").send({user})
+})
+app.post("/users",async (req,res)=>{
+    const user=await User.find(req.body)
+    return res.status("201").send({user})
+})
+app.patch("/users/:id",async(req,res)=>{
+    const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    return res.status("200").send({user})
+})
+app.delete("users/:id",async(req,res)=>{
+    const user=await User.findByIdAndDelete(req.params.id)
+    return res.status("200").send({user})
+
+})
+app.listen(5500,async function(){
+    await connect()
+    console.log("app is litsening on 5500");
 })
 
 
