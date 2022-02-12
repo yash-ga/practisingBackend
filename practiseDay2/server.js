f712d349223a61b524e80c9aeeb8f59d72bdc2f9
Yash Garg <yash_fw10_103@masai.school>
2022-02-12 10:31:50 +0530

    basic crud setup made

---
+++ practiseDay2/server.js
@@ -0,0 +1,37 @@
+// const { json } = require("body-parser")
+const express=require("express")
+const mongoose=require("mongoose")
+const connect=()=>{
+    return mongoose.connect("https/mongo")
+}
+const BookSchema=new mongoose.Schema({
+    bookName:{type:String,required:true},
+    pages:{type:Number,required:true},
+    author:{type:String,required:true},
+    bestseller:{type:Boolean,required:false}
+
+})
+const Book=mongoose.model("books",BookSchema)
+const app=express()
+app.use(express.json())
+
+app.post("/books",async (req,res)=>{
+    const book=await Book.create(req.body)
+    return res.status("200").send({book})
+})
+app.get("/books",async (req,res)=>{
+    const book=await Book.find(req.body)
+    return res.status("200").send({book})
+})
+app.patch("/books/:id",async (req,res)=>{
+    const book=await Book.findByIdAndUpdate(req.params.id,req.body)
+    return res.status("200").send({book})
+})
+app.delete("/books/:id",async (req,res)=>{
+    const book=await Book.findByIdAndDelete(req.params.id)
+    return res.status("200").send({book})
+})
+app.listen(2345,async function(){
+    await connect()
+    console.log("app is litsening on 2345");
+})
