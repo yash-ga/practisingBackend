7192004a1bb573938174c028741109fb60cabdd6
Yash Garg <yash_fw10_103@masai.school>
2022-02-12 10:43:26 +0530

    practising crud operations

--- practiseDay2/server.js
+++ practiseDay2/server.js
@@ -2,7 +2,7 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const connect=()=>{
-    return mongoose.connect("https/mongo")
+    return mongoose.connect("mongodb://127.0.0.1:27017/books")
 }
 const BookSchema=new mongoose.Schema({
     bookName:{type:String,required:true},
